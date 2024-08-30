560ea4345bb3eab360141099086553c86080f587
farjana <farjanaCse74@gmail.com>
2024-08-30 12:01:51 +0000

    data visulization and data post

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,53 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState([]);
-  const [userName, setUserName] = useState([]);
-  const [userEmail, setUserEmail] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  const handleSubmit = ()=>{
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user= {name,email};
+    console.log(user)
+    fetch('http://localhost:5000/users',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log("insite body request",data)
+      const newUser = [...users,data];
+      setUsers(newUser)
+        form.reset();
+      
+
+    })
 
   }
 
   useEffect(() => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
-      .then((data) => setUser(data));
+      .then((data) => setUsers(data));
   }, []);
 
   return (
     <>
-      <h1>Users Management:{user.length}</h1>
+      <h1>Users Management:{users.length}</h1>
       <form onSubmit={handleSubmit}>
-        <input onChange={(e)=>setUserName(e.target.value)} type="text"  name="Rabina" id=""/>
-        <input onChange={(e)=>setUserEmail(e.target.value)} type="email"  name="email" id=""/>
+        <input  type="text"  name="name" id=""/>
+        <br />
+        <input  type="email"  name="email" id=""/>
+        <br />
         <input type="submit"  value="Add User" id=""/>
       </form>
-      {user.map((users) => (
-        <p key={users.id}>
-          {users.name} {users.email}{" "}
+      {users.map((userShow) => (
+        <p key={userShow.id}>
+           {userShow.name}{" "}
         </p>
       ))}
     </>
